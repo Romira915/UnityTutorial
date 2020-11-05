db406091e8bb283ecb2a5a240bc2fe7e9dc2bd14
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2020-11-05 16:08:26 +0900

    不必要なpublicをserializefield化

---
+++ Assets/Scripts/enemy_spawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_spawn : MonoBehaviour
+{
+    [SerializeField] private enemy Enemy;
+    //private float spawn_count = 0;
+    [SerializeField] private float spawn_span = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // コルーチンと同様の処理
+        //spawn_count += Time.deltaTime;
+
+        //if (spawn_count > spawn_span)
+        //{
+        //    GameObject.Instantiate(Enemy.gameObject, transform.position, transform.rotation);
+        //    spawn_count = 0;
+        //}
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            GameObject.Instantiate(Enemy.gameObject, transform.position, transform.rotation);
+            yield return new WaitForSeconds(spawn_span);
+        }
+    }
+}
